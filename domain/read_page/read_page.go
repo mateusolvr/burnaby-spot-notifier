3d3c3e1b28067f8c43bc5de521fa32a4470f0250
mateusolvr <mateus.olvr@gmail.com>
2022-05-04 17:40:00 -0700

    update: read_page adjustments

--- domain/read_page/read_page.go
+++ domain/read_page/read_page.go
@@ -17,6 +17,7 @@ import (
 
 type service struct {
 	validationService domain.ValidationService
+	activities        []domain.Activity
 }
 
 func NewService(validationService domain.ValidationService) *service {
@@ -60,6 +61,11 @@ func (s *service) InitializeCrawler() {
 		}
 		s.clickNextPage(ctx)
 		time.Sleep(2 * time.Second)
+	}
+
+	// Populate day field with time type from string
+	for i, v := range s.activities {
+		s.activities[i].Days = s.validationService.ParseDate(v.DaysStr[:11])
 	}
 
 	fmt.Printf("\nTook: %f secs\n", time.Since(start).Seconds())
@@ -116,14 +122,14 @@ func (s *service) getActivitiesPage(ctx context.Context) {
 			log.Fatal(err)
 		}
 		if s.validationService.ValidateActivity(ctx, firstCol) {
-			s.checkAvailability(ctx, i)
+			s.checkActivityAvailability(ctx, i)
 		}
 
 	}
 
 }
 
-func (s *service) checkAvailability(ctx context.Context, index int) {
+func (s *service) checkActivityAvailability(ctx context.Context, index int) {
 	sel := fmt.Sprintf("/html/body/div[1]/div[2]/div/div[2]/div/div/div[1]/div[2]/div/table/tbody/tr[%d]/td/div[3]/span[1]", index)
 
 	var rows []*cdp.Node
@@ -146,79 +152,51 @@ func (s *service) checkAvailability(ctx context.Context, index int) {
 
 	log.Println("Number rows: ", len(rows))
 
-	// var res string
-	// if len(rows) > 0 {
-	// 	err := chromedp.Run(ctx,
-	// 		chromedp.OuterHTML(fmt.Sprintf(`/html/body/div[1]/div[2]/div/div[2]/div/div/div[1]/div[2]/div/table/tbody/tr[%d]/td/div[3]/div/div/table/tbody/tr`, index), &res),
-	// 	)
-	// 	if err != nil {
-	// 		log.Fatal(err.Error())
-	// 	}
-
-	// 	fmt.Println(res)
-	// }
-
-	s.getDataFromActivityTable(ctx, rows, index)
+	if len(rows) > 0 {
+		s.getDataFromActivityTable(ctx, rows, index)
+	}
 }
 
-func (s *service) getDataFromActivityTable(ctx context.Context, rows []*cdp.Node, index int) (courseName, weekDay, times, days, complexName, availableSpaces string) {
-	const courseNameColumn int = 1
-	const weekDayColumn int = 3
-	const timesColumn int = 4
-	const daysColumn int = 5
-	const complexNameColumn int = 6
-	const availableSpacesColumn int = 8
+func (s *service) getDataFromActivityTable(ctx context.Context, rows []*cdp.Node, index int) {
+	var courseName, weekDay, times, days, complexName, availableSpaces string
 
-	tableSingleRowSel := "/html/body/div[1]/div[2]/div/div[2]/div/div/div[1]/div[2]/div/table/tbody/tr[%d]/td/div[3]/div/div/table/tbody/tr/td[%d]"
-	tableMultRowSel := "/html/body/div[1]/div[2]/div/div[2]/div/div/div[1]/div[2]/div/table/tbody/tr[%d]/td/div[3]/div/div/table/tbody/tr[%d]/td[%d]"
+	rootPath := fmt.Sprintf("/html/body/div[1]/div[2]/div/div[2]/div/div/div[1]/div[2]/div/table/tbody/tr[%d]", index)
+	tableSel := rootPath + "/td/div[3]/div/div/table/tbody/tr%s/td[%d]"
 
-	if len(rows) == 1 {
+	for i := 1; i <= len(rows); i++ {
+		rowIndex := fmt.Sprintf("[%d]", i)
+		if len(rows) == 1 {
+			rowIndex = ""
+		}
+		fmt.Println(fmt.Sprintf(tableSel, rowIndex, domain.CourseNameColumn))
 		err := chromedp.Run(ctx,
-			chromedp.Text(fmt.Sprintf(tableSingleRowSel, index, courseNameColumn), &courseName),
-			chromedp.Text(fmt.Sprintf(tableSingleRowSel, index, weekDayColumn), &weekDay),
-			chromedp.Text(fmt.Sprintf(tableSingleRowSel, index, timesColumn), &times),
-			chromedp.Text(fmt.Sprintf(tableSingleRowSel, index, daysColumn), &days),
-			chromedp.Text(fmt.Sprintf(tableSingleRowSel, index, complexNameColumn), &complexName),
-			chromedp.Text(fmt.Sprintf(tableSingleRowSel, index, availableSpacesColumn), &availableSpaces),
+			chromedp.Text(fmt.Sprintf(tableSel, rowIndex, domain.CourseNameColumn), &courseName),
+			chromedp.Text(fmt.Sprintf(tableSel, rowIndex, domain.WeekDayColumn), &weekDay),
+			chromedp.Text(fmt.Sprintf(tableSel, rowIndex, domain.TimesColumn), &times),
+			chromedp.Text(fmt.Sprintf(tableSel, rowIndex, domain.DaysColumn), &days),
+			chromedp.Text(fmt.Sprintf(tableSel, rowIndex, domain.ComplexNameColumn), &complexName),
+			chromedp.Text(fmt.Sprintf(tableSel, rowIndex, domain.AvailableSpacesColumn), &availableSpaces),
 		)
 		if err != nil {
 			log.Fatal(err)
 		}
-		courseName = s.validationService.CleanString(courseName)
-		weekDay = s.validationService.CleanString(weekDay)
-		times = s.validationService.CleanString(times)
-		days = s.validationService.CleanString(days)
-		complexName = s.validationService.CleanString(complexName)
-		availableSpaces = s.validationService.CleanString(availableSpaces)
+
+		courseName, weekDay, times, days, complexName, availableSpaces = s.validationService.CleanFields(courseName, weekDay, times, days, complexName, availableSpaces)
+		availableSpacesInt, err := strconv.Atoi(availableSpaces)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var newActivity = domain.Activity{
+			CourseName:      courseName,
+			WeekDay:         weekDay,
+			Times:           times,
+			DaysStr:         days,
+			ComplexName:     complexName,
+			AvailableSpaces: availableSpacesInt}
+		s.activities = append(s.activities, newActivity)
 
 		fmt.Printf("Activity: %s\nWeekDay: %s\nTimes: %s\nDays: %s\nComplex Name: %s\nAvailable Spaces: %s\n\n",
 			courseName, weekDay, times, days, complexName, availableSpaces)
 	}
-
-	if len(rows) > 1 {
-		for i := 1; i <= len(rows); i++ {
-			err := chromedp.Run(ctx,
-				chromedp.Text(fmt.Sprintf(tableMultRowSel, index, i, courseNameColumn), &courseName),
-				chromedp.Text(fmt.Sprintf(tableMultRowSel, index, i, weekDayColumn), &weekDay),
-				chromedp.Text(fmt.Sprintf(tableMultRowSel, index, i, timesColumn), &times),
-				chromedp.Text(fmt.Sprintf(tableMultRowSel, index, i, daysColumn), &days),
-				chromedp.Text(fmt.Sprintf(tableMultRowSel, index, i, complexNameColumn), &complexName),
-				chromedp.Text(fmt.Sprintf(tableMultRowSel, index, i, availableSpacesColumn), &availableSpaces),
-			)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			courseName = s.validationService.CleanString(courseName)
-			weekDay = s.validationService.CleanString(weekDay)
-			times = s.validationService.CleanString(times)
-			days = s.validationService.CleanString(days)
-			complexName = s.validationService.CleanString(complexName)
-			availableSpaces = s.validationService.CleanString(availableSpaces)
-
-			fmt.Printf("Activity: %s\nWeekDay: %s\nTimes: %s\nDays: %s\nComplex Name: %s\nAvailable Spaces: %s\n\n",
-				courseName, weekDay, times, days, complexName, availableSpaces)
-		}
-	}
-	return
 }
