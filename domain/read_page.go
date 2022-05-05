68873163166ad8bc9a19f5129c7fcab2a6304c7b
mateusolvr <mateus.olvr@gmail.com>
2022-05-04 17:39:33 -0700

    update: const from columns

--- domain/read_page.go
+++ domain/read_page.go
@@ -1,1 +1,10 @@
 package domain
+
+const (
+	CourseNameColumn      int = 1
+	WeekDayColumn         int = 3
+	TimesColumn           int = 4
+	DaysColumn            int = 5
+	ComplexNameColumn     int = 6
+	AvailableSpacesColumn int = 8
+)
